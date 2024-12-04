ae20f0862dafa6b2496e6a8c563590e68240574e
mkudrenecky <mkudrene@student.ubc.ca>
2024-12-03 21:00:34 -0700

    day3part2: this implementation is heinous but it works, rip

--- day3/day3.rb
+++ day3/day3.rb
@@ -1,12 +1,23 @@
 file = File.open("input.txt")
 file_data = file.read
 file.close
-pattern = /mul\((\d{1,3})\,(\d{1,3})\)/
+pattern = /mul\((\d{1,3}),(\d{1,3})\)|(don't\(\))|(do\(\))/
 matches = file_data.scan(pattern)
+domath = true
 products = matches.map do |pair|
-   pair[0].to_i * pair[1].to_i
+  if pair[3] == "do()"
+    domath = true
+  elsif pair[2] == "don't()"
+    domath = false
+  elsif domath
+    pair[0].to_i * pair[1].to_i
+  end
 end
-sum = products.reduce { |sum, product| sum += product }
-p products
-p matches
+sum = 0
+products.each do |product|
+  if product.is_a?(Integer)
+    sum += product
+  end
+end
 p sum
+
